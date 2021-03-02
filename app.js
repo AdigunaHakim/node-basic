461132e68fd440c147dd86bd6c335667b9858b75
unknown <adiguna.hakim@gmail.com>
2021-03-03 05:28:31 +0700

    serve with seperate path

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const http = require('http');
 const port = 3000;
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    switch (req.url) {
+        case '/' :
+            res.end('This Is Home Page');
+            break;
+        case '/admin' :
+            res.end('This Is Admin Page');
+            break;
+        case '/user' :
+            res.end('This Is User Page');
+            break;
+        default :
+            res.end('404 Not Found')
+            break;
+    } 
+});
 
 server.listen(port, () => {
     console.log(`server is now running on port ${port}`);
