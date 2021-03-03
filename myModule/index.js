0f0c131b9cb6e20e903c0091c101819a5c3f4c6e
unknown <adiguna.hakim@gmail.com>
2021-03-04 06:20:49 +0700

    add local module wrap in index.js

---
+++ myModule/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    sum : require('./sum'),
+    substract : require('./substract')
+}
--- project.js
+++ project.js
@@ -1,3 +1,6 @@
-const { sum, substract } = require('./myModule/math');
+const { sum, substract } = require('./myModule');
+const sumModule = require('./myModule/sum');
 
 console.log(sum(8, 9));
+console.log(substract(10, 9));
+console.log(sumModule(8, 9));
